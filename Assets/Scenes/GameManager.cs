537d83e32ff742e0a8b07a5717ba4cdd2b3fb10e
darianny08 <dariannyo@nycstudents.net>
2025-02-28 12:30:12 -0500

    completed 5.3

--- Assets/Scenes/GameManager.cs
+++ Assets/Scenes/GameManager.cs
@@ -1,31 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    public List<GameObject>targets;
+    public List<GameObject> targets;
     private float spawnRate = 1.0f;
+    public TextMeshProUGUI gameOverText;
+    public Button restartButton;
+
+    public bool isGameActive;
 
     // Start is called before the first frame update
     void Start()
     {
+        isGameActive = true;
         StartCoroutine(SpawnTarget());
+        
+        
     }
 
+
+    public void GameOver()
+    {
+        gameOverText.gameObject.SetActive(true);
+        isGameActive = false;
+        restartButton.gameObject.SetActive(true);
+
+    }
+
+   
     IEnumerator SpawnTarget()
     {
-       while (true)
+
+        while (isGameActive)
        {
-        yield return new WaitForSeconds(spawnRate);
-        int index = Random.Range(0, targets.Count);
-        Instantiate(targets[index]);
-
-       }
-            
-
+            yield return new WaitForSeconds(spawnRate);
+            int index = Random.Range(0, targets.Count);
+            Instantiate(targets[index]);
+        }
     }
 
+    public void RestartGame();
+    {
+        SceneManager.LoadScene(SceneManager.GetActice().name);
+    }
     // Update is called once per frame
     void Update()
     {
--- Assets/Scenes/Target.cs
+++ Assets/Scenes/Target.cs
@@ -10,14 +10,18 @@ public class Target : MonoBehaviour
     private float maxTorque = 10;
     private float xRange = 4;
     private float ySpawnPos = -6;
+    private GameManager gameManager;
+
 
     // Start is called before the first frame update
     void Start()
     {
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
         targetRb  = GetComponent<Rigidbody> ();
         targetRb.AddForce(RandomForce(), ForceMode.Impulse);
         targetRb.AddTorque(RandomTorque(), RandomTorque(), RandomTorque(),ForceMode.Impulse);
         transform.position = RandomSpawnPos();
+
     }
 
 
@@ -37,16 +41,22 @@ public class Target : MonoBehaviour
     }
 
 
-    private void OnMouseDown()
-{
-    Destroy(gameObject);
+  private void OnMouseDown()
+    {
+        if(gameManager.isGameActive) 
+        {
+            Destroy (gameObject);
+        }
+    }
 
-}
-
-private void OnTriggerEnter(Collider other)
-{
-    Destroy(gameObject);
-}
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!gameObject.CompareTag("Bad")) 
+        { 
+            gameManager.GameOver();
+        }
+        Destroy(gameObject);
+    }
      
     
     // Update is called once per frame
